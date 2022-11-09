8f6bcdbdb9b5a0596765e259d042d4099d224e40
Sergey <Sergey@DESKTOP-O9HN6T6>
2022-11-09 21:28:39 +0400

    Add project files.

---
+++ Benchmark/Benchmark/MyParserBenchmark.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Lesson;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    public class MyParserBenchmark
+    {
+        private const string STRING_TO_PARSE_WITH_ERROR = "qwert22";
+        private const string STRING_TO_PARSE = "54";
+
+        private readonly MyParser _myParser = new MyParser();
+
+        [Benchmark]
+        public void TryCatchParse_Test_WithError()
+        {
+            int result = _myParser.TryCatchParse(STRING_TO_PARSE_WITH_ERROR);
+        }
+
+        [Benchmark]
+        public void TryParse_Test_WithError()
+        {
+            int result = _myParser.TryParse(STRING_TO_PARSE_WITH_ERROR);
+        }
+
+        [Benchmark]
+        public void TryParseFixed_Test_WithError()
+        {
+            int result = _myParser.TryParseFixed(STRING_TO_PARSE_WITH_ERROR);
+        }
+        
+        [Benchmark]
+        public void TryParseFixed_V2_Test_WithError()
+        {
+            int result = _myParser.TryParseFixed_V2(STRING_TO_PARSE_WITH_ERROR);
+        }
+        // -----------------------##################_----------------------
+        [Benchmark]
+        public void TryCatchParse_Test()
+        {
+            int result = _myParser.TryCatchParse(STRING_TO_PARSE);
+        }
+
+        [Benchmark]
+        public void TryParse_Test()
+        {
+            int result = _myParser.TryParse(STRING_TO_PARSE);
+        }
+        
+        [Benchmark]
+        public void TryParseFixed_Test()
+        {
+            int result = _myParser.TryParseFixed(STRING_TO_PARSE);
+        }
+
+        [Benchmark]
+        public void TryParseFixed_V2_Test()
+        {
+            int result = _myParser.TryParseFixed_V2(STRING_TO_PARSE);
+        }
+
+    }
+}
---
+++ Benchmark/Lesson/MyParser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson
+{
+    public class MyParser
+    {
+        /// <summary>
+        /// Try Catch is fu*king long and besides that it requires memory in heap for exception
+        /// It's useless to use it for Parsing 
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public int TryCatchParse(string str)
+        {
+            int result;
+
+            try
+            {
+                result = int.Parse(str);
+            }
+            catch (Exception ex)
+            {
+                result = 0;
+            }
+            return result;
+        }
+
+        public int TryParse(string str)
+        {
+            int result;
+            if (!int.TryParse(str, out result))
+            {
+                result = 0;
+            }
+            return result;
+        }
+        public int TryParseFixed(string str) //Best UwU
+        {
+            int result;
+            int.TryParse(str, out result);            
+            return result;
+        }
+        public int TryParseFixed_V2(string str)
+        {
+            int.TryParse(str, out int result);
+            return result;
+        }
+    }
+}
